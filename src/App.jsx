96b63c8b51b7a8661d8b97d8db27e1a230e6d97f
shoumik saha <dipushoumiksaha@gmail.com>
2025-07-28 00:04:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button className='btn btn-primary' > hefkdjk</button>
+       
+    </>
+  )
+}
+
+export default App
